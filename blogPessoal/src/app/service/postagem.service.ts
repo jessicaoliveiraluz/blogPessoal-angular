56159abfd02c0a1a2e6371bef4e384cfda9d09c8
jessica luz <jessiquinha.l.silva@gmail.com>
2020-05-15 13:02:19 -0300

    Criação do endPoint getAllPostagens

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /* 
+  CRUD - Create, ready, Update e Delete.
+  4 operações básicas que um crud faz.
+  */
+
+  getAllPostagens() {
+    return this.http.get('http://31.220.57.14:8080/postagens')
+  }
+
 }
